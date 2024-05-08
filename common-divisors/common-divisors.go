b1f7f8accbae059155551882ec4d4ff2d7c3c476
chazari <chalimovrz@gmail.com>
2024-05-08 19:12:21 +0600

    comments

--- common-divisors/common-divisors.go
+++ common-divisors/common-divisors.go
@@ -3,15 +3,16 @@ package common_divisors
 import "slices"
 
 func CommonDivisors(ints []int) []int {
-	var divisors []int
-
+	// Поиск минимального числа в массиве
 	var m = slices.Min(ints)
+	// Поиск всех делителей числа m
+	var divisors []int
 	for i := 2; i <= m; i++ {
 		divisors = append(divisors, i)
 	}
 
+	// Поиск общих делителей
 	var commonDivisors []int
-
 	for _, divisor := range divisors {
 		if commonDivisorFinder(ints, divisor) {
 			commonDivisors = append(commonDivisors, divisor)
@@ -23,6 +24,7 @@ func CommonDivisors(ints []int) []int {
 
 func commonDivisorFinder(ints []int, divisor int) bool {
 	for _, i := range ints {
+		// Если число не делится на divisor, то это не общий делитель
 		if i%divisor != 0 {
 			return false
 		}
